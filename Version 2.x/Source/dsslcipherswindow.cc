a74924a4625c920f9efe8aef67b0950972a6b3b6
Alexis Megas <textbrowser@gmail.com>
2015-03-27 18:56:22 -0400

    SSL/TLS changes.

--- Version 2.x/Source/dsslcipherswindow.cc
+++ Version 2.x/Source/dsslcipherswindow.cc
@@ -134,16 +134,16 @@ void dsslcipherswindow::populate(void)
 		cipher = QSslCipher
 		  (query.value(0).toString(), QSsl::TlsV1_0);
 #else
-	      cipher = QSslCipher
-		(query.value(0).toString(), QSsl::TlsV1);
+	        cipher = QSslCipher
+		  (query.value(0).toString(), QSsl::TlsV1);
 #endif
 	      else if(protocol.contains("tlsv1.1"))
 #if QT_VERSION >= 0x050000
 		cipher = QSslCipher
 		  (query.value(0).toString(), QSsl::TlsV1_1);
 #else
-	      cipher = QSslCipher
-		(query.value(0).toString(), QSsl::TlsV1);
+	        cipher = QSslCipher
+		  (query.value(0).toString(), QSsl::UnknownProtocol);
 #endif
 #if QT_VERSION >= 0x050000
 	      else if(protocol.contains("tlsv1.2"))
@@ -207,65 +207,9 @@ void dsslcipherswindow::populate(void)
 
   QSqlDatabase::removeDatabase("allowedsslciphers");
 
-  bool sslv3 = false;
-  bool tlsv10 = false;
-  bool tlsv11 = false;
-  bool tlsv12 = false;
-
-  for(int i = 0; i < ui.listWidget->count(); i++)
-    {
-      QListWidgetItem *item = ui.listWidget->item(i);
-
-      if(!item)
-	continue;
-
-      if(item->text().toLower().contains("sslv3"))
-	if(item->checkState() == Qt::Checked)
-	  sslv3 = true;
-
-      if(item->text().toLower().contains("tlsv1.0"))
-	if(item->checkState() == Qt::Checked)
-	  tlsv10 = true;
-
-      if(item->text().toLower().contains("tlsv1.1"))
-	if(item->checkState() == Qt::Checked)
-	  tlsv11 = true;
-
-      if(item->text().toLower().contains("tlsv1.2"))
-	if(item->checkState() == Qt::Checked)
-	  tlsv12 = true;
-    }
-
-  QSsl::SslProtocol protocol = QSsl::SslProtocol(0);
   QSslConfiguration configuration(QSslConfiguration::defaultConfiguration());
 
-#if QT_VERSION >= 0x050000
-  if(sslv3 && tlsv10)
-    protocol = QSsl::TlsV1SslV3;
-  else if(tlsv12)
-    protocol = QSsl::TlsV1_2;
-  else if(tlsv11)
-    protocol = QSsl::TlsV1_1;
-  else if(tlsv10)
-    protocol = QSsl::TlsV1_0;
-  else if(sslv3)
-    protocol = QSsl::SslV3;
-  else
-    protocol = QSsl::UnknownProtocol;
-#else
-  if(sslv3 && tlsv10)
-    protocol = QSsl::TlsV1SslV3;
-  else if(tlsv11 || tlsv12)
-   protocol =  QSsl::UnknownProtocol;
-  else if(tlsv10)
-    protocol = QSsl::TlsV1;
-  else if(sslv3)
-    protocol = QSsl::SslV3;
-  else
-    protocol = QSsl::UnknownProtocol;
-#endif
-
-  configuration.setProtocol(protocol);
+  configuration.setProtocol(QSsl::UnknownProtocol);
   QSslConfiguration::setDefaultConfiguration(configuration);
   QSslSocket::setDefaultCiphers(allowed);
 
