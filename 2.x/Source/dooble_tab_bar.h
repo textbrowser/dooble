3f05b43008f5f56cbfed49c935f9361d1e8f04b7
Monster <textbrowser@gmail.com>
2017-07-18 07:28:22 -0400

    Method not required.

--- 2.x/Source/dooble_tab_bar.h
+++ 2.x/Source/dooble_tab_bar.h
@@ -38,7 +38,6 @@ class dooble_tab_bar: public QTabBar
   dooble_tab_bar(QWidget *parent);
 
  protected:
-  QSize minimumTabSizeHint(int index) const;
   QSize tabSizeHint(int index) const;
 
  private slots:
