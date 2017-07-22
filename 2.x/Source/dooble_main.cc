e850c859d3f01e7fc199bb713bf184bf85ec1b35
Monster <textbrowser@gmail.com>
2017-07-22 13:20:24 -0400

    New widgets.

--- 2.x/Source/dooble_main.cc
+++ 2.x/Source/dooble_main.cc
@@ -207,8 +207,6 @@ int main(int argc, char *argv[])
     (QWebEngineSettings::FullScreenSupportEnabled, true);
   QWebEngineSettings::globalSettings()->setAttribute
     (QWebEngineSettings::LocalContentCanAccessFileUrls, false);
-  dooble_hmac::sha3_512hmac_test1();
-  dooble_hmac::sha3_512hmac_test2();
 
   dooble *d = new dooble();
 
