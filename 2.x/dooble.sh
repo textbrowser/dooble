91203be13edc1be008b5f196b2bfffa40c81eaf9
Monster <textbrowser@gmail.com>
2017-08-20 18:36:54 -0400

    New file.

---
+++ 2.x/dooble.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -r ./Dooble ] && [ -x ./Dooble ]
+then
+    exec ./Dooble -style fusion "$@"
+    exit 1
+elif [ -r /usr/local/dooble/Dooble ] && [ -x /usr/local/dooble/Dooble ]
+then
+    cd /usr/local/dooble && exec ./Dooble -style fusion "$@"
+    exit $?
+else
+    exit 1
+fi
