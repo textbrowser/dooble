6291fb3162faee99e15c3e16948f379baf2e9caf
Monster <textbrowser@gmail.com>
2017-11-09 18:05:29 -0500

    New release.

--- 2.x/Source/dooble.h
+++ 2.x/Source/dooble.h
@@ -37,7 +37,7 @@
 #include "dooble_settings.h"
 #include "ui_dooble.h"
 
-#define DOOBLE_DATE_VERSION_STRING "2017.12.25"
+#define DOOBLE_DATE_VERSION_STRING "2017.11.11"
 #define DOOBLE_VERSION_STRING "2.1"
 
 class QWebEngineDownloadItem;
