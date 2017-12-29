ae04edc2b6571a1264be79290fd2a72c7567f080
Monster <textbrowser@gmail.com>
2017-12-29 18:40:16 -0500

    Clear current index.

--- 2.x/Source/dooble_address_widget_completer.cc
+++ 2.x/Source/dooble_address_widget_completer.cc
@@ -249,6 +249,7 @@ void dooble_address_widget_completer::complete(const QString &text)
 	(qMin(static_cast<int> (dooble_page::MAXIMUM_HISTORY_ITEMS),
 	      m_model->rowCount()) * m_popup->rowHeight(0));
       QCompleter::complete();
+      m_popup->setCurrentIndex(QModelIndex());
     }
   else
     m_popup->setVisible(false);
