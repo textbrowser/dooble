5eddc6ec06d96f111b70b66182905409c25171cf
textbrowser <textbrowser@gmail.com>
2018-01-15 08:51:30 -0500

    Removed focus.

--- 2.x/Source/dooble_page.cc
+++ 2.x/Source/dooble_page.cc
@@ -1298,10 +1298,6 @@ void dooble_page::slot_load_finished(bool ok)
 
   m_ui.reload->setIcon(QIcon(QString(":/%1/36/reload.png").arg(icon_set)));
   m_ui.reload->setToolTip(tr("Reload"));
-
-  if(m_ui.address->hasFocus())
-    m_view->setFocus();
-
   emit iconChanged(icon());
 }
 
