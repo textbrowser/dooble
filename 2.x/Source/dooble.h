1d5542b1505a16fddd23d140ed2cdd3b9d2c073f
Monster <textbrowser@gmail.com>
2018-04-17 07:47:29 -0400

    Stop the jitters.

--- 2.x/Source/dooble.h
+++ 2.x/Source/dooble.h
@@ -37,8 +37,8 @@
 #include "dooble_settings.h"
 #include "ui_dooble.h"
 
-#define DOOBLE_DATE_VERSION_STRING "2018.04.04"
-#define DOOBLE_VERSION_STRING "2.1.9.1"
+#define DOOBLE_DATE_VERSION_STRING "2018.04.30"
+#define DOOBLE_VERSION_STRING "2.1.9.2"
 
 class QWebEngineDownloadItem;
 class QWebEngineProfile;
