9178f048ecae7693cf47c6aeb9fcbae905847a03
Monster <textbrowser@gmail.com>
2018-04-22 10:00:35 -0400

    No big bad bingbang shalalalalabuluboo.

--- 1.56f/dooble.sh
+++ 1.56f/dooble.sh
@@ -3,6 +3,8 @@
 if [ -r /usr/local/dooble/Dooble ] && [ -x /usr/local/dooble/Dooble ]
 then
     export LD_LIBRARY_PATH=/usr/local/dooble/Lib
+    # Disable https://en.wikipedia.org/wiki/MIT-SHM.
+    export QT_X11_NO_MITSHM=1
     cd /usr/local/dooble && exec ./Dooble -style fusion "$@"
     exit $?
 else
