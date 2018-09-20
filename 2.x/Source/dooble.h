684ad621b0f7dc69f6ac4794fd29a00fc167816e
Monster <textbrowser@gmail.com>
2018-09-20 11:22:59 -0400

    Qt 5.11.2.

--- 2.x/Source/dooble.h
+++ 2.x/Source/dooble.h
@@ -37,7 +37,7 @@
 #include "dooble_settings.h"
 #include "ui_dooble.h"
 
-#define DOOBLE_DATE_VERSION_STRING "2018.11.01"
+#define DOOBLE_DATE_VERSION_STRING "2018.09.20"
 #define DOOBLE_VERSION_STRING "2.1.9.4"
 
 class QWebEngineDownloadItem;
