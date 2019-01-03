63332dec17ad5d55160e460f7a26d9b86c6d324c
textbrowser <textbrowser@gmail.com>
2019-01-03 13:28:14 -0500

    New release date.

--- 2.x/Source/dooble.h
+++ 2.x/Source/dooble.h
@@ -37,7 +37,7 @@
 #include "dooble_settings.h"
 #include "ui_dooble.h"
 
-#define DOOBLE_DATE_VERSION_STRING "2019.01.01"
+#define DOOBLE_DATE_VERSION_STRING "2019.01.10"
 #define DOOBLE_VERSION_STRING "2.1.9.5"
 
 class QWebEngineDownloadItem;
