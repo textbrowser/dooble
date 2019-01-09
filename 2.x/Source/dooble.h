542a6002079a01133391ff8b37cb78fa98f504cd
textbrowser@gmail.com <textbrowser@gmail.com>
2019-01-09 08:53:19 -0500

    Removed silly versions.

--- 2.x/Source/dooble.h
+++ 2.x/Source/dooble.h
@@ -37,8 +37,7 @@
 #include "dooble_settings.h"
 #include "ui_dooble.h"
 
-#define DOOBLE_DATE_VERSION_STRING "2019.01.10"
-#define DOOBLE_VERSION_STRING "2.1.9.5"
+#define DOOBLE_VERSION_STRING "2019.01.10"
 
 class QWebEngineDownloadItem;
 class QWebEngineProfile;
