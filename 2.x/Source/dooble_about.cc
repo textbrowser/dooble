77e4f26593c64cfd394199859b440bc7ad5939d6
textbrowser@gmail.com <textbrowser@gmail.com>
2019-01-21 20:34:34 -0500

    New information.

--- 2.x/Source/dooble_about.cc
+++ 2.x/Source/dooble_about.cc
@@ -68,7 +68,7 @@ dooble_about::dooble_about(void):QMainWindow()
     (tr("<a href=\"qrc://Documentation/RELEASE-NOTES.html\">"
 	"Release Notes</a>"));
   m_ui.version->setText
-    (tr("Dooble version %1, Jumping January.").
+    (tr("Dooble version %1, February Fever.").
      arg(DOOBLE_VERSION_STRING));
 }
 
