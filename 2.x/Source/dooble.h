d8fd5817c5f98361e2fab14a252122b474f35a90
textbrowser@gmail.com <textbrowser@gmail.com>
2019-03-17 09:51:53 -0400

    New accepted/blocked domains data file. New release notes.

--- 2.x/Source/dooble.h
+++ 2.x/Source/dooble.h
@@ -37,7 +37,7 @@
 #include "dooble_settings.h"
 #include "ui_dooble.h"
 
-#define DOOBLE_VERSION_STRING "2019.02.15"
+#define DOOBLE_VERSION_STRING "2019.04.04"
 
 class QWebEngineDownloadItem;
 class QWebEngineProfile;
--- 2.x/Source/dooble_about.cc
+++ 2.x/Source/dooble_about.cc
@@ -68,7 +68,7 @@ dooble_about::dooble_about(void):QMainWindow()
     (tr("<a href=\"qrc://Documentation/RELEASE-NOTES.html\">"
 	"Release Notes</a>"));
   m_ui.version->setText
-    (tr("Dooble version %1, February Fever.").
+    (tr("Dooble version %1, April Affection.").
      arg(DOOBLE_VERSION_STRING));
 }
 
