cedda9edba715db6164ac4629fae8935e3851a0a
textbrowser@gmail.com <textbrowser@gmail.com>
2019-07-19 19:53:43 -0400

    Future release.

--- 2.x/Source/dooble.h
+++ 2.x/Source/dooble.h
@@ -38,7 +38,7 @@
 #include "ui_dooble.h"
 #include "ui_dooble_floating_digital_clock.h"
 
-#define DOOBLE_VERSION_STRING "2019.07.07"
+#define DOOBLE_VERSION_STRING "2019.09.09"
 
 class QDialog;
 class QWebEngineDownloadItem;
