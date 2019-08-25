c2a54a9a4587cc3bb76e2974e4d97148ad6efeae
textbrowser@gmail.com <textbrowser@gmail.com>
2019-08-25 17:12:55 -0400

    New icon.

--- 2.x/Source/dooble_tab_widget.cc
+++ 2.x/Source/dooble_tab_widget.cc
@@ -226,7 +226,7 @@ void dooble_tab_widget::prepare_icons(void)
   QString icon_set(dooble_settings::setting("icon_set").toString());
 
   m_add_tab_tool_button->setIcon
-    (QIcon(QString(":/%1/36/add.png").arg(icon_set)));
+    (QIcon(QString(":/%1/18/add.png").arg(icon_set)));
   m_private_tool_button->setIcon
     (QIcon(QString(":/%1/18/private.png").arg(icon_set)));
   m_tabs_menu_button->setIcon
