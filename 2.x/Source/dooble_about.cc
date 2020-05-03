e319ddbb8b90be99a02428af82c461c4ed19d679
textbrowser@gmail.com <textbrowser@gmail.com>
2020-05-03 11:46:18 -0400

    New About.

--- 2.x/Source/dooble_about.cc
+++ 2.x/Source/dooble_about.cc
@@ -68,8 +68,7 @@ dooble_about::dooble_about(void):QMainWindow()
     (tr("<a href=\"qrc://Documentation/RELEASE-NOTES.html\">"
 	"Release Notes</a>"));
   m_ui.version->setText
-    (tr("Dooble version %1, April.").
-     arg(DOOBLE_VERSION_STRING));
+    (tr("Dooble version %1, May Mayhem.").arg(DOOBLE_VERSION_STRING));
 }
 
 void dooble_about::keyPressEvent(QKeyEvent *event)
