e8519d8d0c57102b51c25f473cdd5a7ed8fe72b7
textbrowser@gmail.com <textbrowser@gmail.com>
2021-03-16 16:45:10 -0400

    Chart properties.

--- 2.x/Source/dooble_charts.cc
+++ 2.x/Source/dooble_charts.cc
@@ -85,6 +85,30 @@ properties(void) const
     animationDuration();
   properties[dooble_charts::Properties::ANIMATION_OPTIONS] =
     QVariant::fromValue(m_chart->animationOptions());
+  properties[dooble_charts::Properties::BACKGROUND_COLOR] = m_chart->
+    backgroundBrush().color();
+  properties[dooble_charts::Properties::BACKGROUND_ROUNDNESS] = m_chart->
+    backgroundRoundness();
+  properties[dooble_charts::Properties::BACKGROUND_VISIBLE] = m_chart->
+    isBackgroundVisible();
+  properties[dooble_charts::Properties::CHART_TYPE] = m_chart->chartType();
+  properties[dooble_charts::Properties::DROP_SHADOW_ENABLED] = m_chart->
+    isDropShadowEnabled();
+  properties[dooble_charts::Properties::LOCALE] = m_chart->locale();
+  properties[dooble_charts::Properties::LOCALIZE_NUMBERS] = m_chart->
+    localizeNumbers();
+  properties[dooble_charts::Properties::MARGINS] = QString("%1, %2, %3, %4").
+    arg(m_chart->margins().bottom()).
+    arg(m_chart->margins().left()).
+    arg(m_chart->margins().right()).
+    arg(m_chart->margins().top());
+  properties[dooble_charts::Properties::PLOT_AREA_BACKGROUND_VISIBLE] =
+    m_chart->isPlotAreaBackgroundVisible();
+  properties[dooble_charts::Properties::THEME] = m_chart->theme();
+  properties[dooble_charts::Properties::TITLE] = m_chart->title();
+  properties[dooble_charts::Properties::TITLE_COLOR] =
+    m_chart->titleBrush().color();
+  properties[dooble_charts::Properties::TITLE_FONT] = m_chart->titleFont();
 #endif
   return properties;
 }
