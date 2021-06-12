29304027fdead55bc9013ba5bf7134b6d2eaa462
textbrowser@gmail.com <textbrowser@gmail.com>
2021-06-12 08:12:03 -0400

    JS engine.

--- 2.x/Charts/function_a.js
+++ 2.x/Charts/function_a.js
@@ -1,8 +1,11 @@
-function()
+(function()
 {
-    var array = new Array();
+    var array = new Array(5);
 
-    array[0] = 1.0;
-    array[1] = 2.0;
+    array[0] = 1.1;
+    array[1] = 2.2;
+    array[2] = 3.3;
+    array[3] = 4.4;
+    array[4] = 5.5;
     return array;
-}
+})
