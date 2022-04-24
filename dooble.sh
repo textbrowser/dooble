301dda8a8c82b26c97bba13b93dd36477ffa5236
textbrowser <textbrowser>
2022-04-24 11:37:42 -0400

    Support /usr/local/dooble.

--- dooble.sh
+++ dooble.sh
@@ -10,6 +10,9 @@ export DOOBLE_ADDRESS_WIDGET_HEIGHT_OFFSET=5
 # Maximum of 50.
 
 export DOOBLE_TAB_HEIGHT_OFFSET=5
+
+# Automatic scaling.
+
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 
 # Disable https://en.wikipedia.org/wiki/MIT-SHM.
@@ -18,19 +21,17 @@ export QT_X11_NO_MITSHM=1
 
 if [ -r ./Dooble ] && [ -x ./Dooble ]
 then
-    # Be careful with SSL errors!
-
-    exec ./Dooble \
-	 --disable-reading-from-canvas \
-	 --ignore-certificate-errors \
-	 --ignore-ssl-errors \
-         "$@"
+    exec ./Dooble "$@"
     exit $?
 elif [ -r /opt/dooble/Dooble ] && [ -x /opt/dooble/Dooble ]
 then
     export LD_LIBRARY_PATH=/opt/dooble/Lib
     export QT_PLUGIN_PATH=/opt/dooble/plugins
-    cd /opt/dooble && exec ./Dooble --disable-reading-from-canvas "$@"
+    cd /opt/dooble && exec ./Dooble "$@"
+    exit $?
+elif [ -r /usr/local/dooble/Dooble ] && [ -x /usr/local/dooble/Dooble ]
+then
+    cd /usr/local/dooble && exec ./Dooble "$@"
     exit $?
 else
     exit 1
