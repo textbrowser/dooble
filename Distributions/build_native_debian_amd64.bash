b5955a73ad9b4b6b9f068c42ada4a3d9c9105fec
textbrowser <textbrowser@gmail.com>
2023-09-01 11:43:06 -0400

    Renamed. Not verified.

--- Distributions/build_native_debian_amd64.bash
+++ Distributions/build_native_debian_amd64.bash
@@ -35,7 +35,7 @@ mkdir -p dooble-debian.d/usr/bin
 mkdir -p dooble-debian.d/usr/share/applications
 mkdir -p dooble-debian.d/usr/share/pixmaps
 cp -p dooble.desktop dooble-debian.d/usr/share/applications/.
-cp -pr DEBIAN-NATIVE dooble-debian.d/DEBIAN
+cp -pr UBUNTU-NATIVE-22.04 dooble-debian.d/DEBIAN
 cp -r ./opt/dooble dooble-debian.d/opt/.
 cp Icons/Logo/dooble.png dooble-debian.d/usr/share/pixmaps/.
 cp dooble.sh dooble-debian.d/usr/bin/dooble
