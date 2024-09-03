3612c82f9846d4ffa08944ce58d0ee477a0c000c
textbrowser <textbrowser@gmail.com>
2024-09-03 14:25:31 -0400

    HTTP -> HTTPS.

--- Source/dooble_main.cc
+++ Source/dooble_main.cc
@@ -135,7 +135,8 @@ int main(int argc, char *argv[])
 	      {
 		QUrl url(QUrl::fromUserInput(argv[i]));
 
-		url.setScheme("https");
+		if(url.scheme() == "http")
+		  url.setScheme("https");
 
 		if(dooble_ui_utilities::allowed_url_scheme(url))
 		  urls << url;
@@ -153,7 +154,8 @@ int main(int argc, char *argv[])
 	  {
 	    QUrl url(QUrl::fromUserInput(argv[i]));
 
-	    url.setScheme("https");
+	    if(url.scheme() == "http")
+	      url.setScheme("https");
 
 	    if(dooble_ui_utilities::allowed_url_scheme(url))
 	      urls << url;
