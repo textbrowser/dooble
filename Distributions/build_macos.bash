49de41e5208d2d06ba5d4056ac2196e163d64987
textbrowser <textbrowser@gmail.com>
2024-12-31 19:47:53 -0500

    MacOS.

--- Distributions/build_macos.bash
+++ Distributions/build_macos.bash
@@ -1,28 +1,39 @@
 #!/usr/bin/env bash
 # Alexis Megas.
 
-if [ ! -e dooble.pro ]; then
+if [ ! -e dooble.pro ]
+then
     echo "Please issue $0 from the primary directory."
     exit 1
 fi
 
-if [ ! -x $(which qmake6 2>/dev/null) ]; then
+if [ ! -x $(which qmake6 2>/dev/null) ]
+then
     echo "The qmake6 executable does not exist."
     exit 1
 fi
 
 make distclean 2>/dev/null
-qmake6 -o Makefile dooble.pro
+
+if [ -x ~/Qt/6.8.1/macos/bin/qmake ]
+then
+    ~/Qt/6.8.1/macos/bin/qmake
+else
+    qmake6
+fi
+
 make -j 5
 make install
 make dmg
 
-if [ ! -r Dooble.dmg ]; then
+if [ ! -r Dooble.dmg ]
+then
     echo "Dooble.dmg is not a readable file."
     exit 1
 fi
 
-if [ "$(uname -m)" = "arm64" ]; then
+if [ "$(uname -m)" = "arm64" ]
+then
     mv Dooble.dmg Dooble-2024.12.31_Apple_Silicon.dmg
 else
     mv Dooble.dmg Dooble-2024.12.31_Intel.dmg
