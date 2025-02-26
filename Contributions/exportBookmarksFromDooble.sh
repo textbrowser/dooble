a4f22e98576b2a00120dc975ae3f57ebf9521e2c
textbrowser <textbrowser@gmail.com>
2025-02-26 08:42:34 -0500

    Maybe nicer.

--- Contributions/exportBookmarksFromDooble.sh
+++ Contributions/exportBookmarksFromDooble.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 # Written by publicsite.
 
-if [ "$DOOBLE_HOME" = "" ]; then
+if [ "$DOOBLE_HOME" = "" ]
+then
     DOOBLE_HOME_BOOKMARKS="${HOME}/.dooble"
 else
     DOOBLE_HOME_BOOKMARKS="${DOOBLE_HOME}"
 fi
 
-if [ -f "${DOOBLE_HOME_BOOKMARKS}/dooble_favicons.db" ]; then
+if [ -f "${DOOBLE_HOME_BOOKMARKS}/dooble_favicons.db" ]
+then
     echo '<!DOCTYPE NETSCAPE-Bookmark-file-1>'
     echo '<!-- This is an automatically generated file.'
     echo '     It will be read and overwritten.'
@@ -18,8 +20,12 @@ if [ -f "${DOOBLE_HOME_BOOKMARKS}/dooble_favicons.db" ]; then
     printf "<DL><p>\n"
     printf "    <DT><H3 PERSONAL_TOOLBAR_FOLDER=\"true\">From Dooble</H3>\n"
     printf "    <DL><p>\n"
-    sqlite3 "${DOOBLE_HOME_BOOKMARKS}/dooble_history.db" "SELECT * FROM dooble_history" | while read line; do
-	printf "        <DT><A HREF=\"%s\">%s</A>\n" "$(echo "$line" | cut -d "|" -f 5 | base64 -d -)" "$(printf "%s" "$line" | cut -d "|" -f 4 | base64 -d -)"
+    sqlite3 "${DOOBLE_HOME_BOOKMARKS}/dooble_history.db" \
+	    "SELECT * FROM dooble_history" | while read line
+    do
+	printf "        <DT><A HREF=\"%s\">%s</A>\n" \
+	       "$(echo "$line" | cut -d "|" -f 5 | base64 -d -)" \
+	       "$(printf "%s" "$line" | cut -d "|" -f 4 | base64 -d -)"
     done
     printf "    </DL><p>\n"
     printf "</DL><p>\n"
--- Contributions/importBookmarksIntoDooble.sh
+++ Contributions/importBookmarksIntoDooble.sh
@@ -1,31 +1,41 @@
 #!/bin/sh
 # Written by publicsite.
 
-if [ ! -f "$1" ]; then
+if [ ! -f "$1" ]
+then
     echo "Arg1: bookmarks.html file"
 fi
 
-if [ "$DOOBLE_HOME" = "" ]; then
+if [ "$DOOBLE_HOME" = "" ]
+then
     DOOBLE_HOME_BOOKMARKS="${HOME}/.dooble"
 else
     DOOBLE_HOME_BOOKMARKS="${DOOBLE_HOME}"
 fi
 
-if [ -f "${DOOBLE_HOME_BOOKMARKS}/dooble_favicons.db" ]; then
-    if [ ! -f "${DOOBLE_HOME_BOOKMARKS}/dooble_favicons.db.bak" ]; then
-	cp -a "${DOOBLE_HOME_BOOKMARKS}/dooble_favicons.db" "${DOOBLE_HOME_BOOKMARKS}/dooble_favicons.db.bak"
+if [ -f "${DOOBLE_HOME_BOOKMARKS}/dooble_favicons.db" ]
+then
+    if [ ! -f "${DOOBLE_HOME_BOOKMARKS}/dooble_favicons.db.bak" ]
+    then
+	cp -a "${DOOBLE_HOME_BOOKMARKS}/dooble_favicons.db" \
+	   "${DOOBLE_HOME_BOOKMARKS}/dooble_favicons.db.bak"
     fi
 
-    if [ ! -f "${DOOBLE_HOME_BOOKMARKS}/dooble_history.db.bak" ]; then
-	cp -a "${DOOBLE_HOME_BOOKMARKS}/dooble_history.db" "${DOOBLE_HOME_BOOKMARKS}/dooble_history.db.bak"
+    if [ ! -f "${DOOBLE_HOME_BOOKMARKS}/dooble_history.db.bak" ]
+    then
+	cp -a "${DOOBLE_HOME_BOOKMARKS}/dooble_history.db" \
+	   "${DOOBLE_HOME_BOOKMARKS}/dooble_history.db.bak"
     fi
 
-    grep -o "A HREF.*>" "$1" | while read line; do
+    grep -o "A HREF.*>" "$1" | while read line
+    do
 	link="$(printf "%s" $line | cut -d ">" -f 1 | cut -d "\"" -f 2)"
 	name="$(printf "%s" $line | cut -d ">" -f 2 | cut -d "<" -f 1)"
 
-	if [ "$link" != "" ]; then
-	    if [ "$name" = "" ]; then
+	if [ "$link" != "" ]
+	then
+	    if [ "$name" = "" ]
+	    then
 		name="$link"
 	    fi
 
