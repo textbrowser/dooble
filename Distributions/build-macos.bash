474ef49df78b146ab1986e2c0f34f76f458a0dbb
textbrowser <textbrowser@gmail.com>
2025-07-04 09:47:09 -0400

    MacOS.

--- Distributions/build-macos.bash
+++ Distributions/build-macos.bash
@@ -7,19 +7,14 @@ then
     exit 1
 fi
 
-if [ ! -x $(which qmake6 2>/dev/null) ]
-then
-    echo "The qmake6 executable does not exist."
-    exit 1
-fi
-
 make distclean 2>/dev/null
 
-if [ -x ~/Qt/6.8.1/macos/bin/qmake ]
+if [ -x ~/Qt/6.8.3/macos/bin/qmake ]
 then
-    ~/Qt/6.8.1/macos/bin/qmake
+    ~/Qt/6.8.3/macos/bin/qmake
 else
-    qmake6
+    echo "Please install the official Qt."
+    exit 1
 fi
 
 make -j 5
@@ -32,11 +27,5 @@ then
     exit 1
 fi
 
-if [ "$(uname -m)" = "arm64" ]
-then
-    mv Dooble.dmg Dooble-2025.07.04_Apple_Silicon.dmg
-else
-    mv Dooble.dmg Dooble-2025.07.04_Intel.dmg
-fi
-
+mv Dooble.dmg Dooble-2025.07.04_Universal.dmg
 make distclean 2>/dev/null
