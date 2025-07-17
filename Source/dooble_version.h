eab2a79f3505e149f61dc3f96b3a9e6b2eeabd3b
textbrowser <textbrowser@gmail.com>
2025-07-17 15:20:49 -0400

    New release? Maybe.

--- Source/dooble_version.h
+++ Source/dooble_version.h
@@ -31,7 +31,7 @@
   "textbrowser/dooble/master/Source/dooble_version.h"
 #endif
 #ifndef DOOBLE_VERSION_STRING
-#define DOOBLE_VERSION_STRING "2025.07.04"
+#define DOOBLE_VERSION_STRING "2025.07.17"
 #endif
 #ifndef DOOBLE_VERSION_STRING_LTS
 #define DOOBLE_VERSION_STRING_LTS "2025.07.04"
