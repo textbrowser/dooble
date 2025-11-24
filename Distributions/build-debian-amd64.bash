4a09b25617321a3552436a3caf5466668bf839a3
textbrowser <textbrowser@gmail.com>
2025-11-24 12:25:52 -0500

    Copy WebEngine files.

--- Distributions/build-debian-amd64.bash
+++ Distributions/build-debian-amd64.bash
@@ -24,6 +24,7 @@ mkdir -p ./opt/dooble/Data
 mkdir -p ./opt/dooble/Documentation
 mkdir -p ./opt/dooble/Lib
 mkdir -p ./opt/dooble/Translations
+mkdir -p ./opt/dooble/translations
 cp -p ~/Qt/6.8.3/gcc_64/lib/libQt6Charts.so.6 ./opt/dooble/Lib/.
 cp -p ~/Qt/6.8.3/gcc_64/lib/libQt6Core.so.6 ./opt/dooble/Lib/.
 cp -p ~/Qt/6.8.3/gcc_64/lib/libQt6DBus.so.6 ./opt/dooble/Lib/.
@@ -55,7 +56,8 @@ cp -p ~/Qt/6.8.3/gcc_64/libexec/QtWebEngineProcess ./opt/dooble/.
 cp -pr ~/Qt/6.8.3/gcc_64/plugins ./opt/dooble/.
 cp -pr ~/Qt/6.8.3/gcc_64/resources/* ./opt/dooble/.
 cp -pr ~/Qt/6.8.3/gcc_64/translations/*.qm ./opt/dooble/Translations/.
-cp -pr ~/Qt/6.8.3/gcc_64/translations/qtwebengine_locales ./opt/dooble/.
+cp -pr ~/Qt/6.8.3/gcc_64/translations/qtwebengine_locales \
+   ./opt/dooble/translations/.
 chmod -x,g+w ./opt/dooble/Lib/*
 ~/Qt/6.8.3/gcc_64/bin/qmake -o Makefile dooble.pro && make -j $(nproc)
 cp -p ./Documentation/Documents/*.pdf ./opt/dooble/Documentation/.
