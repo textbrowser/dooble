5944b068d4d8337468607ca92bbdcb86b9c52cd0
textbrowser@gmail.com <textbrowser@gmail.com>
2026-08-20 23:55:17 -0400

    New version.

--- Source/dooble_version.h
+++ Source/dooble_version.h
@@ -31,7 +31,7 @@
   "textbrowser/dooble/master/Source/dooble_version.h"
 #endif
 #ifndef DOOBLE_VERSION_STRING
-#define DOOBLE_VERSION_STRING "2026.08.20"
+#define DOOBLE_VERSION_STRING "2026.08.21"
 #endif
 #ifndef DOOBLE_VERSION_STRING_LTS
 #define DOOBLE_VERSION_STRING_LTS "2026.08.20"
